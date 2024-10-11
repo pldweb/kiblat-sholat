9ff0c0517d1933034a5fe084f53a8b640be16125
Muhammad Rivaldi Fanani <muhammadrivaldifnni01@gmail.com>
2024-10-11 13:36:29 +0700

    Ubah jadi jquery

--- index.php
+++ index.php
@@ -77,39 +77,34 @@
 </div>
 <div id="notice"></div>
 
-
-
 <script>
-            document.addEventListener('DOMContentLoaded', function(event) {
-
+    $(document).ready(function() {
         if (window.DeviceOrientationEvent) {
-        document.getElementById('notice').innerHTML = 'Working API detected';
-        window.addEventListener('deviceorientation', (eventData) => {
-            // gamma: Tilting the device from left to right. Tilting the device to the right will result in a positive value.
-            const tiltLR = eventData.gamma;
-            // beta: Tilting the device from the front to the back. Tilting the device to the front will result in a positive value.
-            const tiltFB = eventData.beta;
-            // alpha: The direction the compass of the device aims to in degrees.
-            const dir = eventData.alpha
-            // Call the function to use the data on the page.
-            deviceOrientationHandler(tiltLR, tiltFB, dir);
-        }, false);
+            $('#notice').html('Working API detected');
+            
+            $(window).on('deviceorientation', function(eventData) {
+                const tiltLR = eventData.originalEvent.gamma;
+                const tiltFB = eventData.originalEvent.beta;
+                const dir = eventData.originalEvent.alpha;
+                deviceOrientationHandler(tiltLR, tiltFB, dir);
+            });
         } else {
-        document.getElementById('notice').innerHTML = 'No API detected'
-        };
-
-        function deviceOrientationHandler(tiltLR, tiltFB, dir) {
-        document.getElementById('tiltLR').innerHTML = Math.ceil(tiltLR);
-        document.getElementById('tiltFB').innerHTML = Math.ceil(tiltFB);
-        document.getElementById('direction').innerHTML = Math.ceil(dir);
-        // Rotate the disc of the compass. - CSS transform
-        const compassDisc = document.getElementById('compassDiscImg');
-        compassDisc.style.transform = `rotate(${dir}deg)`;
-        compassDisc.style.webkitTransform = `rotate(${dir}deg)`;
-        compassDisc.style.MozTransform = `rotate(${dir}deg)`;
+            $('#notice').html('No API detected');
         }
 
-        });
+        function deviceOrientationHandler(tiltLR, tiltFB, dir) {
+            $('#tiltLR').html(Math.ceil(tiltLR));
+            $('#tiltFB').html(Math.ceil(tiltFB));
+            $('#direction').html(Math.ceil(dir));
+
+            const compassDisc = $('#compassDiscImg');
+            compassDisc.css({
+                'transform': `rotate(${dir}deg)`,
+                '-webkit-transform': `rotate(${dir}deg)`,
+                '-moz-transform': `rotate(${dir}deg)`
+            });
+        }
+    });
 </script>
 
 
